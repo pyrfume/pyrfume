43f7b0de64d3a699d1249934580984a6d544c639
Rick Gerkin <rgerkin@asu.edu>
2019-11-07 04:55:46 +0000

    Added Google model module

---
+++ pyrfume/google.py
@@ -0,0 +1,27 @@
+"""Module for interacting with Haddad et al, 2008"""
+
+import os
+import pandas as pd
+from scipy.spatial.distance import pdist, squareform
+from sklearn.preprocessing import StandardScaler
+
+import pyrfume
+from . import features
+from .base import DATA_DIR
+GOOGLE_DIR = DATA_DIR / 'company_x_2019'
+
+
+def get_company_x_embeddings():
+    emb_raw = pyrfume.load_data(GOOGLE_DIR / 'embeddings.csv')
+    emb = emb_raw[[x for x in list(emb_raw) if 'emb_' in x]]
+    emb = emb.dropna()
+    return emb
+
+def get_company_x_distances(embeddings):
+    emb = get_company_x_embeddings()
+    # Compute distance matrix.  
+    # This will give basically the same low-D embedding as using raw high-dimensional embeddings does
+    dist = pdist(emb, metric='euclidean')
+    dist = squareform(dist)
+    distances = pd.DataFrame(dist, index=emb.index, columns=emb.index)
+    return distances
