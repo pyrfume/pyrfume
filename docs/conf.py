5b7e6dd8b7409a44a42602ec792be8e62948c0a6
Rick Gerkin <rgerkin@asu.edu>
2020-03-02 19:11:13 +0000

    Added Readthedocs files

---
+++ docs/conf.py
@@ -0,0 +1,17 @@
+"""Configuration file for the Sphinx documentation builder."""
+
+project = 'Pyrfume'
+copyright = '2019-, Rick Gerkin and Joel Mainland'
+author = 'Rick Gerkin'
+version = '0.1'
+release = '0.1 alpha'
+extensions = ['sphinxcontrib.apidoc',
+              'sphinx.ext.intersphinx',
+              'sphinx.ext.viewcode',
+              'sphinx.ext.githubpages']
+apidoc_module_dir = '../../pyrfume'
+apidoc_output_dir = '../build'
+apidoc_excluded_paths = ['*unit_test*']
+apidoc_separate_modules = True
+exclude_patterns = ['*unit_test*']
+html_theme = 'alabaster'
---
+++ docs/index.rst
@@ -0,0 +1,20 @@
+.. Pyrfume documentation master file, created by
+   sphinx-quickstart on Mon Mar  2 19:02:20 2020.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Pyrfume's documentation!
+===================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
