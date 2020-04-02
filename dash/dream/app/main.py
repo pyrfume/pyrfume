ab06044c07bc5dc6eaeb0d377ee91a270dcb6c63
= <=>
2020-04-02 08:27:09 -0700

    Updated DREAM app

--- dash/dream/app/main.py
+++ dash/dream/app/main.py
@@ -4,11 +4,13 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 from dash_table import DataTable
 import flask
+from flask import send_file
 import numpy as np
 import pyrfume
-from pyrfume.odorants import smiles_to_image, from_cids
+from pyrfume.odorants import smiles_to_image, from_cids, cids_to_smiles
 from pyrfume.predictions import load_dream_model, smiles_to_features, predict
-
+import pathlib
+OUTPUT_DIR = pathlib.Path(__file__).parent / 'data'
 
 ##### Initialize app #####
 
@@ -39,9 +41,10 @@ def make_odorant_selector():
 def make_descriptors_table(data=None):
     """Make a data table for the given space"""
     if data is None:
-        data = [{desc: None for desc in descriptors}]
+        data = [{i: None for i in descriptors}]
     return DataTable(id='model_output',
-                     columns=[{"name": i, "id": i} for i in descriptors],
+                     columns=[{"name": 'SMILES', "id": 'smiles'}]+\
+                             [{"name": i, "id": i} for i in descriptors],
                      data=data,
                      style_header={'transform': 'translate(0px, 0px) rotate(-0.0deg)',
                                    'max-width': '20px',
@@ -62,9 +65,19 @@ def make_elements(data=None):
 def get_model_output(smiles):
     if not isinstance(smiles, list):
         smiles = [smiles]
+    cids = [int(s) for s in smiles if s.isnumeric()]
+    if cids:
+        x = cids_to_smiles(cids)
+        print(cids, x)
+        smiles = [x[int(s)] if s.isnumeric() else s for s in smiles]
     features = smiles_to_features(smiles, use_features, imputer)
     predictions = predict(model, features, descriptors).reset_index()
+    predictions['smiles'] = smiles
     print(predictions.shape, predictions.mean(), len(descriptors))
+    import os
+    print(os.getcwd())
+    print(OUTPUT_DIR / 'dream.csv')
+    predictions.set_index('smiles').to_csv(OUTPUT_DIR / 'dream.csv')
     return predictions
 
 ###### App layout ######
@@ -75,28 +88,40 @@ dapp.layout = html.Div(className='container-fluid', children=[
                  children=[make_odorant_selector()])
         ]),
 
-    html.Div(className='row', children=[
-        html.Div(className='col', children=[
-            html.Table([
-                html.Tr([html.Td(['Name:']),
-                         html.Td(id='name')]),
-                ], style={'vertical-align': 'middle',
-                          'font-size': '160%'}),
-            html.Img(id='molecule-2d', src=''), ])
-        ]),
+    #html.Div(className='row', children=[
+    #    html.Div(className='col', children=[
+    #        html.Table([
+    #            html.Tr([html.Td(['Name:']),
+    #                     html.Td(id='name')]),
+    #            ], style={'vertical-align': 'middle',
+    #                      'font-size': '160%'}),
+    #        html.Img(id='molecule-2d', src=''), ])
+    #    ]),
 
     html.Div(className='row', children=[
         html.Div(className='col',
                  children=[make_descriptors_table()])
         ]),
-    ]
-)
+    
+    html.Div(className='row', children=[
+        html.Div(className='col',
+                 children=[html.A('Download', href='download')])
+        ]),
+])
+
+
+@dapp.server.route('/download') 
+def download_csv():
+    return send_file(OUTPUT_DIR / 'dream.csv',
+                     mimetype='text/csv',
+                     attachment_filename='dream.csv',
+                     as_attachment=True)
 
 ##### App callbacks #####
 
 outputs = []
-outputs.append(Output('name', 'children'))
-outputs.append(Output('molecule-2d', 'src'))
+#outputs.append(Output('name', 'children'))
+#outputs.append(Output('molecule-2d', 'src'))
 outputs.append(Output('model_output', 'data'))
 
 
@@ -105,10 +130,11 @@ outputs.append(Output('model_output', 'data'))
     [Input('smiles', 'value')],
     )
 def select_cid(smiles):
-    result = ['']*3
-    result[0] = 'Something'
-    result[1] = get_image_src(smiles)
-    result[2] = get_model_output(smiles).to_dict('records')
+    smiles = smiles.split(',')
+    result = ['']*1
+    #result[0] = 'Something'
+    #result[1] = get_image_src(smiles[0])
+    result[0] = get_model_output(smiles).to_dict('records')
     return result
 
 
