180ad04325cbdaad75910566b26289cda3bb2a75
Richard C Gerkin <rgerkin@asu.edu>
2020-05-27 09:19:01 -0700

    Update setup.py for pip>=20

--- setup.py
+++ setup.py
@@ -1,39 +1,30 @@
 """Setup file for Pyrfume"""
 
-import sys
-import os
-
-try:
-    from pip.req import parse_requirements
-    from pip.download import PipSession
-except ImportError:
-    from pip._internal.req import parse_requirements
-    try:
-        from pip._internal.download import PipSession
-    except ImportError:
-        from pip._internal.network.session import PipSession
-
+from pathlib import Path
 from setuptools import setup, find_packages
 
-# IPython 6.0+ does not support Python 2.6, 2.7, 3.0, 3.1, or 3.2
-if sys.version_info < (3, 3):
-    ipython = "ipython>=5.1,<6.0"
-else:
-    ipython = "ipython>=5.1"
 
 def read_requirements():
-    '''parses requirements from requirements.txt'''
-    reqs_path = os.path.join('.', 'requirements.txt')
-    install_reqs = parse_requirements(reqs_path, session=PipSession())
-    reqs = [str(ir.req) for ir in install_reqs]
+    """Parses requirements from requirements.txt"""
+    reqs_path = Path(__file__).parent / 'requirements.txt'
+    reqs = None
+    with open(reqs_path) as reqs_file:
+        reqs = reqs_file.read().splitlines()
     return reqs
+
 
 def get_version():
     version = {}
-    with open("pyrfume/version.py") as f:
+    with open(Path(__file__).parent / 'pyrfume' / 'version.py') as f:
         exec(f.read(), version)
     return version['__version__']
 
+
+readme_path = Path(__file__).parent / 'README.md'
+with open(readme_path, encoding='utf-8') as f:
+    long_description = f.read()
+
+    
 setup(
     name='pyrfume',
     version=get_version(),
@@ -42,10 +33,8 @@ setup(
     packages=find_packages(),
     url='http://pyrfume.scidash.org',
     license='MIT',
-    description='A valdiation library for human olfactory psychophysics research.',
+    long_description=long_description,
+    description='A validation library for human olfactory psychophysics research.',
     install_requires=read_requirements(),
     extras_require={'features': ['rdkit']}
 )
-
-
-
