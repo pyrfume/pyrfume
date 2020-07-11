a9da38fe2acc536ca0166a662a3d804571b27cc3
Zhiwei Liang <zhiwei.liang@asu.edu>
2020-07-10 20:34:56 -0700

    Add test_config_data

--- pyrfume/unit_test/__main__.py
+++ pyrfume/unit_test/__main__.py
@@ -1,6 +1,7 @@
 import sys
 import unittest
 
+from .test_config_data import *
 def main():
     buffer = 'buffer' in sys.argv
     sys.argv = sys.argv[:1] # :Args need to be removed for __main__ to work.  
