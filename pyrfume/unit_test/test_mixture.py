89febf4e12fade47f057fb7c84dc8543b00bf310
Zhiwei Liang <zhiwei.liang@asu.edu>
2020-07-15 17:58:42 -0700

    Add unit tests

--- pyrfume/unit_test/test_mixture.py
+++ pyrfume/unit_test/test_mixture.py
@@ -3,8 +3,11 @@ from pyrfume import Mixture, Component
 
 class MixtureTestCase(unittest.TestCase):
     def test_constructor(self):
-        mixture = Mixture(0)
-        mixture = Mixture(9999)
-
+        mixture1 = Mixture(0)
+        mixture2 = Mixture(9999)
+        self.assertEqual(mixture1.N, 0)
+        self.assertEqual(mixture1.r(mixture2), 0)
+        self.assertEqual(mixture1.overlap(mixture2), 0)
+        self.assertEqual(mixture1.hamming(mixture2), 0)
 if __name__ == '__main__':
     unittest.main()
