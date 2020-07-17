8d319b7704bd6b7856e4892c327883918473db52
Zhiwei Liang <zhiwei.liang@asu.edu>
2020-07-16 18:37:06 -0700

    More unit test cases

--- pyrfume/unit_test/test_mixture.py
+++ pyrfume/unit_test/test_mixture.py
@@ -1,4 +1,5 @@
 import unittest
+import quantities as pq
 from datetime import datetime
 from pyrfume import Mixture, Component
 from pyrfume.odorants import Compound, ChemicalOrder, Vendor, Molecule
@@ -11,13 +12,23 @@ class MixtureTestCase(unittest.TestCase):
         self.assertEqual(mixture1.overlap(mixture2), 0)
         self.assertEqual(mixture1.hamming(mixture2), 0)
 
+    def test_example(self):
         cid = 21946271
         cas = "12002-61-8"
         vendor = Vendor("Test_Vendor", "https://en.wikipedia.org/wiki/Actinium(III)_oxide")
         molecule = Molecule(cid, "test_mol", False)
         molecule = Molecule(cid, "test_mol", True)
+        self.assertIsNone(molecule.molarity)
+        molecule.molecular_weight = pq.Quantity(502.1, pq.g / pq.mol)
+        molecule.density = pq.Quantity(501.9982, pq.g / pq.cm ** 3)
+        self.assertAlmostEqual(molecule.molarity.item(), 999.797251, 3)
+
         chemical_order = ChemicalOrder(molecule, vendor, "", 0.9, None)
         compound = Compound(chemical_order, "TEST", datetime.now, datetime.now, False)
+
+        self.assertIsInstance(getattr(compound, "vendor"), Vendor)
+        self.assertIsInstance(getattr(compound, "cid"), int)
+
         component = Component(cid, "test_name", cas, 0.5, compound)
         mixture = Mixture(100, [component])
 
