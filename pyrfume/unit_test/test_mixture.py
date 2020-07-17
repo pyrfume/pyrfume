5244f649f928da0b9a20cf7d303a371533fac36a
Zhiwei Liang <zhiwei.liang@asu.edu>
2020-07-17 16:52:54 -0700

    Add more tests

--- pyrfume/unit_test/test_mixture.py
+++ pyrfume/unit_test/test_mixture.py
@@ -30,7 +30,32 @@ class MixtureTestCase(unittest.TestCase):
         self.assertIsInstance(getattr(compound, "cid"), int)
 
         component = Component(cid, "test_name", cas, 0.5, compound)
-        mixture = Mixture(100, [component])
+        component1 = Component(cid, "test_name", cas, 0.8, compound)
+        mixture = Mixture(1, [component])
+
+        components_vector = mixture.components_vector(mixture.components, 1)
+        self.assertEqual(components_vector.size, 1)
+
+        mixture1 = Mixture(0)
+        self.assertIsNone(mixture1.r(mixture))
+        self.assertEqual(mixture.overlap(mixture, True), 100.0)
+        
+        mixture.add_component(component1)
+        mixture.remove_component(component1)
+        self.assertEqual(len(mixture.components), 1)
+        mixture.remove_component(component)
+        self.assertEqual(len(mixture.components), 0)
+
+        cas_descriptor = {cas : ["test description"]}
+        cas_descriptor1 = {cas : {"test description key" : 1}}
+        component.set_descriptors("unittest", cas_descriptor)
+        component1.set_descriptors("unittest", cas_descriptor1)
+        mixture.add_component(component)
+        mixture.add_component(component1)
+
+        descriptors_list = mixture.descriptor_list("unittest")
+        self.assertTrue('test description key' in descriptors_list)
+        self.assertTrue('test description' in descriptors_list)
 
 if __name__ == '__main__':
     unittest.main()
