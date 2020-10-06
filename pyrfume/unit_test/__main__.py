d04cb6114cc01c4c5c766918fe7a7b9d416e5570
Zhiwei Liang <zhiwei.liang@asu.edu>
2020-10-05 17:05:00 -0700

    Fix unit tests

--- pyrfume/unit_test/__main__.py
+++ pyrfume/unit_test/__main__.py
@@ -8,13 +8,16 @@ from .test_test import *
 from .test_others_in__init__ import *
 from .test_physics import *
 from .test_odorants import *
+from .test_datajoint import DataJointTestCase
 
 def main():
     buffer = 'buffer' in sys.argv
-    if '-d' in sys.argv:
-        from .test_datajoint import DataJointTestCase
-        
-    sys.argv = sys.argv[:2] # :Args need to be removed for __main__ to work.  
+    database = 'database' in sys.argv
+    sys.argv = sys.argv[:1]
+
+    if not database:
+        globals().pop('DataJointTestCase')
+
     unittest.main(buffer=buffer)
 
 if __name__ == '__main__':
