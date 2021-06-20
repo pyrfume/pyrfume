5eaa2da6159c516f6ac49921479f95ee721c643a
Richard C Gerkin <rgerkin@asu.edu>
2021-06-19 20:29:49 -0700

    Update url.py

--- url.py
+++ url.py
@@ -1,6 +1,6 @@
 import requests
 
 url = "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/8857/property/MolecularWeight,IsomericSMILES/JSON"
-response = request.get(url)
+response = requests.get(url)
 print(response.status_code)
 print(response.content)
